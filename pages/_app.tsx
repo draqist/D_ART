955b51f6d19ab3b54d8e6a7aea1452f6bd4a8a4d
Draqode <Boomjay008@gmail.com>
2022-08-22 14:29:33 +0100

    fea: added web fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AnimatePresence exitBeforeEnter>
       <ChakraProvider theme={theme}>
-        <Fonts />
+        {/* <Fonts /> */}
         <Component {...pageProps} />
       </ChakraProvider>
     </AnimatePresence>
